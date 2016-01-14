c460f6950a1c68ed38491a9b227f78e1255c36d7
Jk Jensen <azjkjensen@gmail.com>
2016-01-13 17:37:39 -0700

    Testing file reading and adding add() functionality

--- spell_corrector/SpellCorrector.java
+++ spell_corrector/SpellCorrector.java
@@ -1,10 +1,35 @@
 package spell_corrector;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by Jordan on 1/13/2016.
  */
-public class SpellCorrector {
-    public SpellCorrector(){
+public class SpellCorrector implements ISpellCorrector{
+    Trie dictionary = new Trie();
+    String bestWord = null;
+    int bestFrequency;
 
+    @Override
+    public void useDictionary(String dictionaryFileName) throws IOException {
+        Scanner scanner = new Scanner(new BufferedReader(new FileReader(new File(dictionaryFileName))));
+
+          //TESTING FILE READING
+//        for(int i = 0; i < 100; i++){
+//            System.out.println(scanner.next());
+//        }
+        while(scanner.hasNext()){
+            dictionary.add(scanner.next());
+        }
+//        System.out.println("Words: " + dictionary.getWordCount() + "\nNodes: " + dictionary.getNodeCount());
+    }
+
+    @Override
+    public String suggestSimilarWord(String inputWord) throws NoSimilarWordFoundException {
+        return "WOOHOO";
     }
 }
--- spell_corrector/Trie.java
+++ spell_corrector/Trie.java
@@ -3,23 +3,76 @@ package spell_corrector;
 /**
  * Created by Jordan on 1/13/2016.
  */
-public class Trie {
+public class Trie implements ITrie {
     private static final int NUMBER_OF_CHILDREN = 26;
 
-    private Node[] children = new Node[NUMBER_OF_CHILDREN];
+    private int wordCount;
+    private int nodeCount;
+    Node rootNode = new Node();
 
     public Trie(){
-
+        wordCount = 0;
+        nodeCount = 0;
     }
 
-    public Node[] getChildren() {
-        return children;
+//    public String toString(){
+//        StringBuilder output = new StringBuilder("TESTING, TESTING");
+//        output.append("\r\n");
+//
+//
+//        return output.toString();
+//    }
+
+    @Override
+    public void add(String word) {
+//        System.out.println("Adding " + word);
+        String wordLower = word.toLowerCase();
+        Node currentNode = rootNode;
+        for(int i = 0; i < wordLower.length(); i++){
+            int numValOfChar = wordLower.charAt(i) - 'a';
+//            System.out.println(wordLower.charAt(i));
+            if(currentNode.children[numValOfChar] == null){
+//                System.out.println("Adding char " + wordLower.charAt(i));
+                currentNode.children[numValOfChar] = new Node();
+                nodeCount++;
+            }
+            currentNode = currentNode.children[numValOfChar];
+            if(i == wordLower.length() - 1){
+                if(currentNode.frequency == 0){
+                    wordCount++;
+                }
+                currentNode.frequency++;
+//                System.out.println("  Incrementing " + wordLower + " to " + currentNode.frequency);
+            }
+        }
     }
 
+    @Override
+    public INode find(String word) {
+        return null;
+    }
 
-    public class Node {
-        public Node(){
+    @Override
+    public int getWordCount() {
+        return wordCount;
+    }
 
+    @Override
+    public int getNodeCount() {
+        return nodeCount;
+    }
+
+    public class Node implements INode{
+        Node[] children = new Node[NUMBER_OF_CHILDREN];
+        int frequency = 0;
+
+        public Node[] getChildren() {
+            return children;
+        }
+
+        @Override
+        public int getValue() {
+            return frequency;
         }
     }
 }
