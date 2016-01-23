1a06464924e131946e160c26527812c371638dff
Jk Jensen <azjkjensen@gmail.com>
2016-01-23 13:17:45 -0700

    Completed(?) Hangman Project, ready for passoff.

--- hangman/EvilHangmanGame.java
+++ hangman/EvilHangmanGame.java
@@ -3,25 +3,45 @@ package hangman;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collector;
 
 /**
  * Created by Jordan on 1/22/2016.
  */
 public class EvilHangmanGame implements IEvilHangmanGame {
-    HashSet<String> words = new HashSet<>();
+    private HashSet<String> words = new HashSet<>();
+    private TreeSet<String> guessed = new TreeSet<>();
+    private HashMap<String, Set<String>> partitions = new HashMap<>();
+    private String partialWord;
 
+    //Empty constructor
+    public EvilHangmanGame(){
+        partialWord = "";
+    }
+
+    public String getPartialWord() {
+        return partialWord;
+    }
+
+    public TreeSet<String> getGuessed() {
+        return guessed;
+    }
+
+    //Starts the game.
     @Override
     public void startGame(File dictionary, int wordLength) {
-        readDictionary(dictionary);
+        guessed = new TreeSet<>();
+        createPartialWord(wordLength);
+        readDictionary(dictionary); //Read in word file for use as current dictionary
+        HashSet<String> partitionedWords = new HashSet<>(); //Set for only words of length wordLength
         for(String str : words){
             if(str.length() == wordLength){
-                //Do something to keep these words.
+                partitionedWords.add(str);
             }
         }
-
+        words = partitionedWords;
+        System.out.println(partitionedWords.toString());
     }
 
     private void readDictionary(File dictionary){
@@ -30,22 +50,144 @@ public class EvilHangmanGame implements IEvilHangmanGame {
 
             words.clear(); //Empty dictionary to read in a new one.
             while(scanner.hasNext()){
-                words.add(scanner.next()); //Read all words into set.
+                String current = scanner.next();
+                if(current.matches("[a-zA-Z]+")) {
+                    words.add(scanner.next()); //Read all words into set.
+                }
             }
         }
         catch(Exception e){
-            System.out.println("Error with reading dictionary" + e.getMessage());
+            System.out.println("Error with reading dictionary " + e.getMessage());
         }
 
+    }
+
+    private void createPartialWord(int wordLength){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < wordLength; i++){
+            sb.append("-");
+        }
+        partialWord = sb.toString();
+    }
+
+    private String key(String word, char currChar){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < word.length(); i++){
+            char ch = word.charAt(i);
+            if(ch == currChar) sb.append(currChar);
+            else sb.append("-");
+        }
+//        System.out.println(sb.toString());
+        return sb.toString();
     }
 
     @Override
     public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
         if(guessMade(guess)) throw new GuessAlreadyMadeException();
-        return null;
+        partitions.clear();
+        guessed.add(Character.toString(guess));
+        for(String currWord : words){
+            String currentKey = key(currWord, guess);
+            if(partitions.containsKey(currentKey)){
+                partitions.get(currentKey).add(currWord);
+            } else {
+                HashSet<String> newSet = new HashSet<>();
+                newSet.add(currWord);
+                partitions.put(currentKey, newSet);
+            }
+        }
+
+        String bestKey = "";
+        Set<String> bestSet = new HashSet<>();
+        for(String currentKey : partitions.keySet()){
+            Set<String> currSet = partitions.get(currentKey);
+            if(currSet.size() > bestSet.size()){
+                bestSet = currSet;
+                bestKey = currentKey;
+            }
+            else if(currSet.size() == bestSet.size()){
+                if(containsNoGuesses(currentKey)) {
+                    bestKey = currentKey;
+                    bestSet = currSet;
+                    break;
+                }
+
+                if(getNumberOfGuessedLetters(currentKey) < getNumberOfGuessedLetters(bestKey)){
+                    bestKey = currentKey;
+                    bestSet = currSet;
+                    break;
+                }
+                if(!containsNoGuesses(bestKey)) {
+                    if (rightMostIndexCompare(currentKey, bestKey)) {
+                        bestKey = currentKey;
+                        bestSet = currSet;
+                        break;
+                    }
+                }
+
+                if(bestKey == ""){
+                    System.out.println("No best key found");
+                }
+            }
+        }
+        partialWord = unionKeys(bestKey, partialWord);
+        words = (HashSet<String>) bestSet;
+        return bestSet;
+    }
+
+    private String unionKeys(String key1, String key2){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < key1.length(); i++){
+            if(key1.charAt(i) == '-') sb.append(key2.charAt(i));
+            else sb.append(key1.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    private boolean containsNoGuesses(String word){
+        for(int i = 0; i < word.length(); i++){
+            if(word.charAt(i) != '-') return false;
+        }
+        return true;
+    }
+
+    private int getNumberOfGuessedLetters(String word){
+        int count = 0;
+        for(int i = 0; i < word.length(); i++){
+            if(word.charAt(i) != '-') count++;
+        }
+        return count;
+    }
+
+    private boolean rightMostIndexCompare(String word1, String word2){
+        int index1 = 0;
+        int index2 = 0;
+        for(int i = 0; i < word1.length(); i++){
+            if(word1.charAt(i) != '-') index1 = i;
+            if(word2.charAt(i) != '-') index2 = i;
+        }
+        if(index1 == index2) {
+            return rightMostIndexCompare(word1.substring(0, index1), word2.substring(0, index2));
+        }
+        return index1 < index2;
     }
 
     private boolean guessMade(char guess){
-        return false;
+        if(guessed.contains(Character.toString(guess))) return true;
+        else return false;
+    }
+
+    public boolean gameWin(){
+        for(int i = 0; i < partialWord.length(); i++){
+            if(partialWord.charAt(i) == '-') return false;
+        }
+        return true;
+    }
+
+    public String getAWord() throws Exception{
+        for(String str : words) {
+            return str;
+        }
+        throw new Exception("Error, word set was empty");
     }
 }
--- hangman/Main.java
+++ hangman/Main.java
@@ -1,8 +1,59 @@
 package hangman;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * Created by Jordan on 1/22/2016.
  */
 public class Main {
-    EvilHangmanGame hangman = new EvilHangmanGame();
+
+    public static void main(String[] args) {
+        try{
+            File dictionary = new File(args[0]); //File for words in dictionary
+            int wordLength = Integer.parseInt(args[1]); //Length of word to be guessed
+            int numberOfGuesses = Integer.parseInt(args[2]); //Number of guesses that the user gets
+
+            EvilHangmanGame hangman = new EvilHangmanGame();
+
+            hangman.startGame(dictionary, wordLength);
+
+            Scanner inputReader = new Scanner(System.in);
+            for(int i = numberOfGuesses;i > 0; i--){
+                System.out.println("You have " + i + " guesses left");
+                String read = "";
+                while(true) {
+                    System.out.print("Enter guess: ");
+                    read = inputReader.next();
+                    if(read.matches("[a-zA-Z]")) break;
+                }
+                char guessedChar = read.charAt(0);
+                try {
+                    hangman.makeGuess(guessedChar);
+                    TreeSet<String> guessedChars = hangman.getGuessed();
+                    System.out.print("Used letters: ");
+                    for(String str : guessedChars){
+                        System.out.print(str + " ");
+                    }
+                    System.out.println();
+                } catch (IEvilHangmanGame.GuessAlreadyMadeException ex){
+                    System.out.println("Sorry, you already guessed that!\n");
+                    i++;
+                }
+                System.out.println("Word: " + hangman.getPartialWord());
+                if(hangman.gameWin()) break;
+            }
+            if(hangman.gameWin()){
+                System.out.println("Congratulations! The word was " + hangman.getPartialWord());
+            } else {
+                System.out.println("GAME OVER");
+                System.out.println("Sorry the word was " + hangman.getAWord());
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
