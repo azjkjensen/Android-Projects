a1c829f0df2b20d37461a01b2e92bc21a986646f
Jk Jensen <azjkjensen@gmail.com>
2016-02-22 21:13:40 -0700

    Added TODO for SM.

--- SuperAsteroids/app/src/main/java/edu/byu/cs/superasteroids/ship_builder/ShipBuilderStates.java
+++ SuperAsteroids/app/src/main/java/edu/byu/cs/superasteroids/ship_builder/ShipBuilderStates.java
@@ -4,7 +4,7 @@ package edu.byu.cs.superasteroids.ship_builder;
  * Created by Jk on 2/19/2016.
  */
 public class ShipBuilderStates {
-
+    //TODO: Ask the TA's about implementing this. http://vanillajava.blogspot.com/2011/06/java-secret-using-enum-as-state-machine.html
     interface State {
         IShipBuildingView.PartSelectionView partSelectionView = null;
     }
