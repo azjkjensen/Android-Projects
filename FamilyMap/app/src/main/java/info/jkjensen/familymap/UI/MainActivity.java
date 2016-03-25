d0a62432e657fbc0919c82f3079b100e67701bd8
Jk Jensen <azjkjensen@gmail.com>
2016-03-25 14:52:48 -0600

    Changed mainActivity to inherit from appcompatactivity

--- FamilyMap/app/src/main/java/info/jkjensen/familymap/UI/MainActivity.java
+++ FamilyMap/app/src/main/java/info/jkjensen/familymap/UI/MainActivity.java
@@ -3,13 +3,16 @@ package info.jkjensen.familymap.UI;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import info.jkjensen.familymap.Model.FamilyMap;
 import info.jkjensen.familymap.R;
 import info.jkjensen.familymap.UI.Fragments.LoginFragment;
 import info.jkjensen.familymap.UI.Fragments.MapFragment;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     private static final String MAP_FRAGMENT_TAG = "mapfragment";
 
@@ -35,6 +38,19 @@ public class MainActivity extends FragmentActivity {
 
         mMapFragment = new MapFragment();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+//        getMenuInflater().inflate(R.menu.fragment_crime_list, menu);
+//
+//        MenuItem subtitleItem = menu.findItem(R.id.menu_item_show_subtitle);
+//        if(mSubtitleVisible){
+//            subtitleItem.setTitle(R.string.hide_subtitle);
+//        } else {
+//            subtitleItem.setTitle(R.string.show_subtitle);
+//        }
     }
 
     public void onLogin(){
