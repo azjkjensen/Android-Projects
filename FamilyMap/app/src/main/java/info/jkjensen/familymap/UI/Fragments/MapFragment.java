c5b50ea4124af52257c3dcbdd306c891286e4a5f
Jk Jensen <azjkjensen@gmail.com>
2016-03-27 11:23:13 -0600

    Fixed sizing issues on MapFragment.

--- FamilyMap/app/src/main/java/info/jkjensen/familymap/UI/Fragments/MapFragment.java
+++ FamilyMap/app/src/main/java/info/jkjensen/familymap/UI/Fragments/MapFragment.java
@@ -26,19 +26,19 @@ public class MapFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_map, container, false);
 
 
-        DisplayMetrics displayMetrics = getActivity().getResources().getDisplayMetrics();
-        float dpHeight = displayMetrics.heightPixels / displayMetrics.density;
-        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
-
-        mMapLayout = v.findViewById(R.id.map);
-        ViewGroup.LayoutParams mapParams =  mMapLayout.getLayoutParams();
-//        mapParams.height = (int)((7f/8f) * dpHeight);
-//        mMapLayout.setLayoutParams(mapParams);
-
-        mPersonLayout = v.findViewById(R.id.layout_person);
-        ViewGroup.LayoutParams personParams =  mPersonLayout.getLayoutParams();
-//        personParams.height = (int)((1f/8f) * dpHeight);
-//        mPersonLayout.setLayoutParams(personParams);
+//        DisplayMetrics displayMetrics = getActivity().getResources().getDisplayMetrics();
+//        float dpHeight = displayMetrics.heightPixels / displayMetrics.density;
+//        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+//
+//        mMapLayout = v.findViewById(R.id.map);
+//        ViewGroup.LayoutParams mapParams =  mMapLayout.getLayoutParams();
+////        mapParams.height = (int)((7f/8f) * dpHeight);
+////        mMapLayout.setLayoutParams(mapParams);
+//
+//        mPersonLayout = v.findViewById(R.id.layout_person);
+//        ViewGroup.LayoutParams personParams =  mPersonLayout.getLayoutParams();
+////        personParams.height = (int)((1f/8f) * dpHeight);
+////        mPersonLayout.setLayoutParams(personParams);
 
         return v;
     }
