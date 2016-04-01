a304c0693d34ae8b98349dacb027e09124ee5bac
Jk Jensen <azjkjensen@gmail.com>
2016-04-01 08:49:13 -0600

    Added person activity

---
+++ FamilyMap/app/src/main/java/info/jkjensen/familymap/UI/PersonActivity.java
@@ -0,0 +1,20 @@
+package info.jkjensen.familymap.UI;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import info.jkjensen.familymap.R;
+
+public class PersonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person);
+    }
+
+}
